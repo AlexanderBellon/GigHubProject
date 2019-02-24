dd123c13a95ce988a898b30a24da75f998809d6d
Alexander Bellon <a.bellon@gmx.de>
2019-02-24 17:58:18 +0100

    Move DateTime calculation to GigFormView Model.

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,20 +1,23 @@
-﻿using System.Linq;
-using GigHub.Models;
-using System.Web.Mvc;
+﻿using GigHub.Models;
 using GigHub.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-       
+
         private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
-        _context = new ApplicationDbContext();
+            _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -23,5 +26,30 @@ namespace GigHub.Controllers
             };
             return View(viewModel);
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+           
+        
+
+            var gig = new Gig
+
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse
+                           (string.Format("{0} {1}",
+                           viewModel.Date, 
+                           viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
+    
 }
